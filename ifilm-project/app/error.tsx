93cafb8abd4ae518dc8fd9fc160b5c0a759813ec
Nkosi <masangonim@gmail.com>
2025-03-09 19:40:15 +0200

    handle the error

--- ifilm-project/app/error.tsx
+++ ifilm-project/app/error.tsx
@@ -1,13 +1,18 @@
 "use client";
 
-import React from "react";
+import { useEffect } from "react";
 
-export default function Error({ error, reset }: { error: Error; reset: () => void }) {
+export default function error({ error, reset })
+ { useEffect(() => {
+  console.log(error);
+ }, [error]);
   return (
-    <div>
+    <div className="text-center mt-10">
       <h1>Something went wrong!</h1>
       <p>{error.message}</p>
-      <button onClick={() => reset()}>Try again</button>
+      <button className="hover:text-amber-600" onClick={() => reset()}>
+        Try again
+        </button>
     </div>
   );
 }
