3707df2b52185f25e4d001920de3facf05522162
Nkosi <masangonim@gmail.com>
2025-03-10 16:29:41 +0200

    add Cardand modified for it to show results

--- ifilm-project/app/page.tsx
+++ ifilm-project/app/page.tsx
@@ -3,24 +3,33 @@ import React from 'react'
 
 const API_KEY = process.env.API_KEY;
 
-export default async function Home({searchParams}) {
+interface SearchParams {
+  genre?: string;
+}
+
+interface HomeProps {
+  searchParams: SearchParams;
+}
+
+export default async function Home({searchParams}: HomeProps) {
   const genre = searchParams.genre || 'fetchTrending';
   const res = await fetch(
     `https://api.themoviedb.org/3${
-      genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`}?api_key=${API_KEY}&
-      language=en-US&page=1`,
+      genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
+    }?api_key=${API_KEY}&language=en-US&page=1`,
       { next: { revalidate: 60}}
     );
-    const data = await res.json();
+    
     if(!res.ok){
-      throw new Error('Faild to fetch data')
+      throw new Error('Failed to fetch data')
     }
+    const data = await res.json();
     const results = data.results;
     console.log(results);
-    
+     
   return (
     <div>
-      <Results results={results} />
+      <Results result={results} />
     </div>
   )
 }
